f67e6c7740b484a22bd7817c8372eb64dd820c9e
Gillian Hammett <gillerbiller@gmail.com@hackbrightacademy.com>
2020-04-28 16:10:46 -0700

    guessing game with invalid input feature added

--- game.py
+++ game.py
@@ -3,39 +3,34 @@ import random
 
 greeting = input("Hello! Please enter your name ")
 
-print (f"Hi {greeting}, I'm thinking of number between 1 and 100.")
-
+print (f"Hi {greeting}, I'm thinking of number between 1 and 100. Try and \
+guess which number I am thinking of.")
 
 def gameplay():
 
     numberOfGuesses = 0
 
     number = (random.choice(range(1, 100)))
-    print(number)
 
     while number:
-
-        guessedNumber = int(input("Pick a number. "))
+        try:
+            guessedNumber = int(input("Pick a number. "))
+        except ValueError:
+            print("Not a valid selection")
+            gameplay()
 
         if guessedNumber < number:
             numberOfGuesses += 1
-            print(f"To low, {guessedNumber}")
+            print(f"That number is too low, {guessedNumber}")
 
         elif guessedNumber > number:
             numberOfGuesses += 1
-            print(f"to high, {guessedNumber}")
+            print(f"that number is too high, {guessedNumber}")
 
         else:
             guessedNumber == number
-            print(f"That is my number, you guess {numberOfGuesses} time(s)")
+            print(f"That is my number! It took you {numberOfGuesses} \
+guesses to guess my number.")
             break
 gameplay()
 
-
-  
-        
-
-
-
-
-# Put your code here
