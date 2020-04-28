3691388eae30ef26d787069239cc7b8df637a8b3
Gillian Hammett <gillerbiller@gmail.com@hackbrightacademy.com>
2020-04-28 14:11:43 -0700

    commit1

--- game.py
+++ game.py
@@ -3,8 +3,29 @@ import random
 
 playerName = input("Hello! Please enter your name ")
 
-print(f"Hi {playerName}, I'm thinking of number between 1 and 100")
+print (f"Hi {playerName}, I'm thinking of number between 1 and 100.")
 
-number = random.choice(range(100))
-print(number)
+def gamePlay():
+    number = (random.choice(range(100)))
+    print(number)
+
+    guessedNumber = int(input("Pick a number. "))
+
+    counter = 0
+
+    for x in number:
+        counter += 1
+        if number < guessedNumber:
+            print(input("That number is too high. Guess again. "))
+        elif number > guessedNumber:
+            print(input("That number is too low. Guess again "))
+        else:
+            number = guessedNumber
+            print(f"That is the number I was thinking of! You guessed \
+            it in only {counter} trys.")
+        
+gamePlay()
+
+
+
 # Put your code here
